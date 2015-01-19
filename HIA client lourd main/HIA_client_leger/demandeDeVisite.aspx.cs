d2b47a067442a4452ab5018a99f0eeed5b3e74bf
loutyl <t.maalem@aforp.eu>
2015-01-19 15:35:03 +0100

    Ajout d'une #region dans initPageControl()
    Ajout d'une #region contenant l'ajout des placeholders des textbox de la
    page demandeDeVisite

--- HIA client lourd main/HIA_client_leger/demandeDeVisite.aspx.cs
+++ HIA client lourd main/HIA_client_leger/demandeDeVisite.aspx.cs
@@ -21,8 +21,16 @@ namespace HIA_client_leger
         {
             //Ajout de la class "active" à la li de la master page            
             HtmlGenericControl liItem = (HtmlGenericControl)Master.FindControl("demandeDeVisite");
-            liItem.Attributes.Add("class", "active");           
-            
+            liItem.Attributes.Add("class", "active");
+
+            #region init textBox etape1 placeholder
+            txtBoxNomPatient.Attributes.Add("placeholder", "Nom du patient");
+            txtBoxPrenPatient.Attributes.Add("placeholder", "Prénom du patient");
+            txtBoxCodePatient.Attributes.Add("placeholder", "Exemple : A12B45");
+            txtBoxEtagePatient.Attributes.Add("placeholder", "Numéro de l'étage");
+            txtBoxChambrePatient.Attributes.Add("placeholder", "Numéro de la chambre");
+            txtBoxLitPatient.Attributes.Add("placeholder", "Localisation du lit");
+            #endregion
         }
     
     }
