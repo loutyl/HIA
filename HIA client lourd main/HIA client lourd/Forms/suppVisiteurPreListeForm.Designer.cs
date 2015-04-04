08208169c64bb873a30387860f0f916b13bc35aa
loutyl <t.maalem@aforp.eu>
2015-04-04 15:59:56 +0200

    Ajout fonctionalité de suppression d'un visiteur d'une pre-liste

---
+++ HIA client lourd main/HIA client lourd/Forms/suppVisiteurPreListeForm.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace HIA_client_lourd.Forms
+{
+    partial class suppVisiteurPreListeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChercherVisiteurSupp = new System.Windows.Forms.Button();
+            this.txtBoxNomVisiteurSupp = new System.Windows.Forms.TextBox();
+            this.txtBoxEmailVisiteurSupp = new System.Windows.Forms.TextBox();
+            this.lblNomVisiteurSupp = new System.Windows.Forms.Label();
+            this.lblEmailVisiteurSupp = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnSuppVisiteur = new System.Windows.Forms.Button();
+            this.lblMsgConfirmationSupp = new System.Windows.Forms.Label();
+            this.panelInfoVisiteurSupp = new System.Windows.Forms.Panel();
+            this.panelInfoVisiteurSupp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnChercherVisiteurSupp
+            // 
+            this.btnChercherVisiteurSupp.Location = new System.Drawing.Point(397, 31);
+            this.btnChercherVisiteurSupp.Name = "btnChercherVisiteurSupp";
+            this.btnChercherVisiteurSupp.Size = new System.Drawing.Size(135, 23);
+            this.btnChercherVisiteurSupp.TabIndex = 0;
+            this.btnChercherVisiteurSupp.Text = "Chercher";
+            this.btnChercherVisiteurSupp.UseVisualStyleBackColor = true;
+            this.btnChercherVisiteurSupp.Click += new System.EventHandler(this.btnChercherVisiteurSupp_Click);
+            // 
+            // txtBoxNomVisiteurSupp
+            // 
+            this.txtBoxNomVisiteurSupp.Location = new System.Drawing.Point(12, 33);
+            this.txtBoxNomVisiteurSupp.Name = "txtBoxNomVisiteurSupp";
+            this.txtBoxNomVisiteurSupp.Size = new System.Drawing.Size(137, 20);
+            this.txtBoxNomVisiteurSupp.TabIndex = 1;
+            // 
+            // txtBoxEmailVisiteurSupp
+            // 
+            this.txtBoxEmailVisiteurSupp.Location = new System.Drawing.Point(216, 33);
+            this.txtBoxEmailVisiteurSupp.Name = "txtBoxEmailVisiteurSupp";
+            this.txtBoxEmailVisiteurSupp.Size = new System.Drawing.Size(137, 20);
+            this.txtBoxEmailVisiteurSupp.TabIndex = 2;
+            // 
+            // lblNomVisiteurSupp
+            // 
+            this.lblNomVisiteurSupp.AutoSize = true;
+            this.lblNomVisiteurSupp.Location = new System.Drawing.Point(13, 14);
+            this.lblNomVisiteurSupp.Name = "lblNomVisiteurSupp";
+            this.lblNomVisiteurSupp.Size = new System.Drawing.Size(86, 13);
+            this.lblNomVisiteurSupp.TabIndex = 3;
+            this.lblNomVisiteurSupp.Text = "Nom du visiteur :";
+            // 
+            // lblEmailVisiteurSupp
+            // 
+            this.lblEmailVisiteurSupp.AutoSize = true;
+            this.lblEmailVisiteurSupp.Location = new System.Drawing.Point(216, 14);
+            this.lblEmailVisiteurSupp.Name = "lblEmailVisiteurSupp";
+            this.lblEmailVisiteurSupp.Size = new System.Drawing.Size(81, 13);
+            this.lblEmailVisiteurSupp.TabIndex = 4;
+            this.lblEmailVisiteurSupp.Text = "Adresse e-mail :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nom :";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Prénom :";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(113, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Adresse e-mail :";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(113, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "label6";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 120);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Téléphone :";
+            this.label7.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(113, 120);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "label8";
+            this.label8.Visible = false;
+            // 
+            // btnSuppVisiteur
+            // 
+            this.btnSuppVisiteur.Location = new System.Drawing.Point(262, 110);
+            this.btnSuppVisiteur.Name = "btnSuppVisiteur";
+            this.btnSuppVisiteur.Size = new System.Drawing.Size(232, 23);
+            this.btnSuppVisiteur.TabIndex = 13;
+            this.btnSuppVisiteur.Text = "Supprimer";
+            this.btnSuppVisiteur.UseVisualStyleBackColor = true;
+            this.btnSuppVisiteur.Visible = false;
+            this.btnSuppVisiteur.Click += new System.EventHandler(this.btnSuppVisiteur_Click);
+            // 
+            // lblMsgConfirmationSupp
+            // 
+            this.lblMsgConfirmationSupp.AutoSize = true;
+            this.lblMsgConfirmationSupp.Location = new System.Drawing.Point(259, 85);
+            this.lblMsgConfirmationSupp.Name = "lblMsgConfirmationSupp";
+            this.lblMsgConfirmationSupp.Size = new System.Drawing.Size(35, 13);
+            this.lblMsgConfirmationSupp.TabIndex = 14;
+            this.lblMsgConfirmationSupp.Text = "label9";
+            this.lblMsgConfirmationSupp.Visible = false;
+            // 
+            // panelInfoVisiteurSupp
+            // 
+            this.panelInfoVisiteurSupp.Controls.Add(this.label2);
+            this.panelInfoVisiteurSupp.Controls.Add(this.lblMsgConfirmationSupp);
+            this.panelInfoVisiteurSupp.Controls.Add(this.btnSuppVisiteur);
+            this.panelInfoVisiteurSupp.Controls.Add(this.label1);
+            this.panelInfoVisiteurSupp.Controls.Add(this.label3);
+            this.panelInfoVisiteurSupp.Controls.Add(this.label8);
+            this.panelInfoVisiteurSupp.Controls.Add(this.label4);
+            this.panelInfoVisiteurSupp.Controls.Add(this.label7);
+            this.panelInfoVisiteurSupp.Controls.Add(this.label5);
+            this.panelInfoVisiteurSupp.Controls.Add(this.label6);
+            this.panelInfoVisiteurSupp.Location = new System.Drawing.Point(12, 68);
+            this.panelInfoVisiteurSupp.Name = "panelInfoVisiteurSupp";
+            this.panelInfoVisiteurSupp.Size = new System.Drawing.Size(516, 144);
+            this.panelInfoVisiteurSupp.TabIndex = 15;
+            this.panelInfoVisiteurSupp.Visible = false;
+            // 
+            // suppVisiteurPreListeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 231);
+            this.Controls.Add(this.panelInfoVisiteurSupp);
+            this.Controls.Add(this.lblEmailVisiteurSupp);
+            this.Controls.Add(this.lblNomVisiteurSupp);
+            this.Controls.Add(this.txtBoxEmailVisiteurSupp);
+            this.Controls.Add(this.txtBoxNomVisiteurSupp);
+            this.Controls.Add(this.btnChercherVisiteurSupp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "suppVisiteurPreListeForm";
+            this.Text = "Supprimer un visiteur";
+            this.Load += new System.EventHandler(this.suppVisiteurPreListeForm_Load);
+            this.panelInfoVisiteurSupp.ResumeLayout(false);
+            this.panelInfoVisiteurSupp.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChercherVisiteurSupp;
+        private System.Windows.Forms.TextBox txtBoxNomVisiteurSupp;
+        private System.Windows.Forms.TextBox txtBoxEmailVisiteurSupp;
+        private System.Windows.Forms.Label lblNomVisiteurSupp;
+        private System.Windows.Forms.Label lblEmailVisiteurSupp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnSuppVisiteur;
+        private System.Windows.Forms.Label lblMsgConfirmationSupp;
+        private System.Windows.Forms.Panel panelInfoVisiteurSupp;
+    }
+}
---
+++ HIA client lourd main/HIA client lourd/Forms/suppVisiteurPreListeForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using databaseHIA;
+
+namespace HIA_client_lourd.Forms
+{
+    public partial class suppVisiteurPreListeForm : Form
+    {
+        private Patient _patientRecherche;
+        private static string _databaseConnectionString = ConfigurationManager.ConnectionStrings["dbConnectionString"].ConnectionString;
+        private List<string> _infoVisiteur;
+
+        public suppVisiteurPreListeForm(Patient currentPatient)
+        {
+            this._patientRecherche = currentPatient;
+
+            InitializeComponent();
+        }
+        private void suppVisiteurPreListeForm_Load(object sender, EventArgs e)
+        {
+            this.Height = 107;
+        }
+
+        private void btnChercherVisiteurSupp_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(this.txtBoxNomVisiteurSupp.Text) || !String.IsNullOrEmpty(this.txtBoxEmailVisiteurSupp.Text))
+            {
+                this._infoVisiteur = new List<string>();
+
+                heavyClientDatabaseObject hdb = new heavyClientDatabaseObject(suppVisiteurPreListeForm._databaseConnectionString);
+
+                _infoVisiteur = hdb.searchVisiteurinPreList(this.txtBoxNomVisiteurSupp.Text, this.txtBoxEmailVisiteurSupp.Text, Convert.ToInt32(this._patientRecherche._IdPatient));
+
+                if (_infoVisiteur.Count > 0)
+                {
+                    this.label2.Text = this._infoVisiteur[1];
+                    this.label4.Text = this._infoVisiteur[2];
+                    this.label6.Text = this._infoVisiteur[3];
+                    if (String.IsNullOrEmpty(this._infoVisiteur[4]))
+                    {
+                        this.label8.Text = "Non renseigné.";
+                    }
+                    else
+                    {
+                        this.label8.Text = this._infoVisiteur[4];
+                    }
+
+                    foreach (Control control in this.panelInfoVisiteurSupp.Controls)
+                    {
+                        if (control.Name == this.lblMsgConfirmationSupp.Name)
+                        {
+                            control.Visible = false;
+                        }
+                        else
+                        {
+                            control.Visible = true;
+                        }
+
+                    }
+
+                    panelInfoVisiteurSupp.Visible = true;
+
+                    this.Height = 260;
+                }
+                else
+                {
+                    MessageBox.Show("Aucun visiteur n'a été trouvé.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez entrer le nom et l'adresse e-mail du visiteur que vous souhaitez supprimer.");
+            }
+        }
+
+        private void btnSuppVisiteur_Click(object sender, EventArgs e)
+        {
+            btnSuppVisiteur = sender as Button;
+
+            heavyClientDatabaseObject hdb = new heavyClientDatabaseObject(suppVisiteurPreListeForm._databaseConnectionString);
+
+            if (hdb.deleteFromPreList(this._infoVisiteur[1], this._infoVisiteur[3], Convert.ToInt32(this._infoVisiteur[0])))
+            {
+                this.lblMsgConfirmationSupp.Text = "Le visiteur a correctement été supprimer.";
+                this.lblMsgConfirmationSupp.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("Il y a eu un problème lors de la suppression du visiteur, veuillez réessayer ultérieurement.");
+            }
+        }
+    }
+}
